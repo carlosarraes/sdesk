2b8e362642f72e7c00f5caa976cbb06cefa452bf
carraes <carraeshb@gmail.com>
2022-10-05 12:08:01 -0300

    Backend

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-console.log('teste');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome!' });
+});
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'));
+
+app.listen(PORT, () => console.log(`Listening - PORT: ${PORT}`));
