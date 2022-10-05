f5b80d4b0d13ded7631ddd7652752a290369e100
carraes <carraeshb@gmail.com>
2022-10-05 16:42:21 -0300

    handlers

--- backend/routes/controllers/userController.js
+++ backend/routes/controllers/userController.js
@@ -1,11 +1,23 @@
-const registerUser = (req, res) => {
+const asyncHandler = require('express-async-handler');
+
+// /api/users - Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error('Please include all fields');
+  }
+
   res.send('Register Route');
-};
+});
 
-const loginUser = (req, res) => {
+// /api/users/login - Public
+const loginUser = asyncHandler(async (req, res) => {
   res.send('Login Route');
-};
+});
 
+//setitut:tutz1234
 module.exports = {
   registerUser,
   loginUser,
