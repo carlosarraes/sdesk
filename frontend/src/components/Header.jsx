903d462749278b0c25178700f3f11d05eec1d59e
carraes <carraeshb@gmail.com>
2022-10-07 00:31:30 -0300

    user done

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,23 +1,45 @@
 import { FaSignInAlt, FaUser, FaSignOutAlt } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { logout, reset } from '../features/auth/authSlice';
 
 const Header = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  function onLogout() {
+    dispatch(logout());
+    dispatch(reset());
+    navigate('/');
+  }
+
+  const { user } = useSelector((state) => state.auth);
   return (
     <header className='header'>
       <div className='logo'>
         <Link to='/'>Support Desk</Link>
       </div>
       <ul>
-        <li>
-          <Link to='/login'>
-            <FaSignInAlt /> Login
-          </Link>
-        </li>
-        <li>
-          <Link to='/register'>
-            <FaUser /> Register
-          </Link>
-        </li>
+        {user ? (
+          <li>
+            <button className='btn' onClick={onLogout}>
+              <FaSignOutAlt /> Logout
+            </button>
+          </li>
+        ) : (
+          <>
+            <li>
+              <Link to='/login'>
+                <FaSignInAlt /> Login
+              </Link>
+            </li>
+            <li>
+              <Link to='/register'>
+                <FaUser /> Register
+              </Link>
+            </li>
+          </>
+        )}
       </ul>
     </header>
   );
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,98 @@
+import { useState, useEffect } from 'react';
+import { toast } from 'react-toastify';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { FaSignInAlt } from 'react-icons/fa';
+import { login, reset } from '../features/auth/authSlice';
+import Spinner from '../components/Spinner';
+
 const Login = () => {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = formData;
+
+  function onChange(e) {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  }
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+
+    if (isSuccess && user) {
+      navigate('/');
+    }
+
+    dispatch(reset());
+  }, [isError, isSuccess, user, message, navigate, dispatch]);
+
+  function onSubmit(e) {
+    e.preventDefault();
+    const userData = {
+      email,
+      password,
+    };
+
+    dispatch(login(userData));
+  }
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  return (
+    <>
+      <section className='heading'>
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+        <p>Please log in to get support</p>
+      </section>
+      <section className='form'>
+        <form onSubmit={onSubmit}>
+          <div className='form-group'>
+            <input
+              type='email'
+              id='email'
+              name='email'
+              className='form-control'
+              value={email}
+              onChange={onChange}
+              placeholder='Enter email'
+              required
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              id='password'
+              name='password'
+              className='form-control'
+              value={password}
+              onChange={onChange}
+              placeholder='Enter password'
+              required
+            />
+          </div>
+          <div className='form-group'>
+            <button className='btn btn-block'>Log in</button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 };
 
 export default Login;
